8e8d041cd33732495c8e9b7036f3c714ee4f8a34
Marcus Absher <marcus.absher@gmail.com>
2025-10-01 14:12:57 -0700

    created src directory and init file and utils file

---
+++ src/myapp/__init__.py
@@ -0,0 +1,3 @@
+"""My Python Application Package."""
+
+__version__ = "0.1.0"
---
+++ src/myapp/utils.py
@@ -0,0 +1,13 @@
+"""Utility functions for the application."""
+
+def add(a, b):
+    """Add two numbers."""
+    return a + b
+
+def multiply(a, b):
+    """Multiply two numbers."""
+    return a * b
+
+def format_name(first_name, last_name):
+    """Format a full name."""
+    return f"{first_name} {last_name}".title()
